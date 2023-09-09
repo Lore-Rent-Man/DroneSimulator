cd27f61e21c3d71517b341ffd26f76ca96ba087d
Laurent Man <laurent.tian.man@gmail.com>
2023-09-08 20:48:06 -0700

    Updated all the formulas, need to now come up with the correct set of values for realistic simulation

--- DroneSimulator/Motor.h
+++ DroneSimulator/Motor.h
@@ -7,12 +7,8 @@ class Motor
 private:
 
 	float baseCurrent;
-	float kTor; //Torque proportionality constant
-	float kBla; //Blade configuration constant
-	float kVol; //Back-emf per rpm
-	float A;	//Area swept out under rotor
-	float PA; //Propeller cross-section
-	float Cd; //Dimensionless constant
+	float k;
+	float b;
 	float Radius;
 
 public:
@@ -20,26 +16,14 @@ public:
 	float inputCurrent;
 	int motorNumber;
 
-	float calculateAngularVelocity() {
-		return 0.0;
-	}
-
-	float calculateTorque()
+	float calcThrust(float aVel)
 	{
-		return kTor * (inputCurrent - baseCurrent);
+		return k * pow(aVel, 2);
 	}
 
-	float calculatePower(double aVel)
+	float calcTorque(float aVel)
 	{
-		return kVol / kTor * calculateTorque() * aVel;
-	}
-
-	float calculateThrust(float p, float aVel) {
-		return pow((kVol * kBla * sqrt(2 * p * A)) / kTor * calculateAngularVelocity(), 2);
-	}
-
-	float calculateTorqueDrag(float p, float aVel) {
-		return 0.5 * Radius * p * Cd * A * (aVel * Radius);
+		return b * pow(aVel, 2);
 	}
 };
 
--- DroneSimulator/Quadcopter.h
+++ DroneSimulator/Quadcopter.h
@@ -13,13 +13,22 @@ class Quadcopter
 private:
 	glm::vec3 m_force{};
 	glm::vec3 m_torque{};
-
+	float gravity = 9.81;
 public:
-	double mass = 1.0f;
+	float mass = 1.0f;
+	float frictionConstant = 0.016f;
 	glm::vec3 position{};
 	glm::quat orientation{};
 	glm::vec3 velocity{};
 	glm::vec3 angular_velocity{};
+	glm::vec3 acceleration{};
+	glm::vec3 angular_acceleration{};
+	glm::mat3 inertia{
+		1.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 1.0f
+	};
+
 	glm::mat4 MVP{};
 	pair<int, Motor> motors[4] = {};
 
@@ -41,19 +50,79 @@ public:
 	Line cr1 = Line(cross[0], cross[1]);
 	Line cr2 = Line(cross[2], cross[3]);
 
-	glm::vec3 calculateFriction(float frictionConstant)
+	glm::vec3 calculateThrustVector(glm::vec4 aVel)
 	{
-		return -frictionConstant * velocity;
+		float totalThrust = 0.0f;
+		for (int i = 0; i < 4; i++)
+		{
+			totalThrust += motors[i].second.calcThrust(aVel[i]);
+		}
+		return glm::vec3{ 0.0f, totalThrust, 0.0f };
 	}
 
-	glm::vec3 updateTorque(float p, float aVel)
+	glm::vec3 calculateTorqueVector(glm::vec4 aVel)
 	{
-		glm::vec3 u_torque{};
-		for (int i = 0; i < 4; i++) {
-			u_torque[0] += motors[i].second.calculateThrust(p, aVel) * motors[i].first;
-			u_torque[1] += motors[i].second.calculateThrust(p, aVel) * motors[i].first;
-			u_torque[2] += motors[i].second.calculateTorqueDrag(p, aVel);
+		float torque_x = glm::length(cross[0]) * motors[0].second.calcThrust(aVel[0]) - glm::length(cross[2]) * motors[2].second.calcThrust(aVel[2]);
+		float torque_y = glm::length(cross[1]) * motors[1].second.calcThrust(aVel[1]) - glm::length(cross[3]) * motors[3].second.calcThrust(aVel[3]);
+		float torque_z = 0;
+		for (int i = 0; i < 4; i++)
+		{
+			torque_z += motors[i].second.calcTorque(aVel[i]);
 		}
+	}
+
+	glm::vec3 calculateAcceleration(glm::vec4 aVel)
+	{
+		glm::vec3 g { 0.0f, -gravity, 0.0f };
+		glm::vec3 f = velocity * frictionConstant;
+		glm::vec3 T = glm::mat3_cast(orientation) * calculateThrustVector(aVel);
+		return g + 1 / mass * T + f;
+	}
+
+	glm::vec3 AngularVelocityToTimeDerivative(glm::vec3 a)
+	{
+		glm::vec3 euler_angles = glm::eulerAngles(orientation);
+		float phi = euler_angles[0];
+		float theta = euler_angles[1];
+		float psi = euler_angles[2];
+		glm::mat3 W{
+			1.0f, 0.0f, -sin(theta),
+			0.0f, cos(phi), cos(theta) * sin(phi),
+			0.0f, -sin(phi), cos(theta) * cos(phi)
+		};
+		return glm::inverse(W) * a;
+	}
+
+	glm::vec3 TimeDerivativeToAngularVelocity(glm::vec3 t)
+	{
+		glm::vec3 euler_angles = glm::eulerAngles(orientation);
+		float phi = euler_angles[0];
+		float theta = euler_angles[1];
+		float psi = euler_angles[2];
+		glm::mat3 W{
+			1.0f, 0.0f, -sin(theta),
+			0.0f, cos(phi), cos(theta) * sin(phi),
+			0.0f, -sin(phi), cos(theta) * cos(phi)
+		};
+		return W * t;
+	}
+
+	glm::vec3 calculateAngularAcceleration(glm::vec4 aVel)
+	{
+		glm::vec3 torque = calculateTorqueVector(aVel);
+		return glm::inverse(inertia) * (torque - glm::cross(angular_velocity, inertia * angular_velocity));
+	}
+
+	void update(glm::vec4 input, float dt)
+	{
+		glm::vec3 a = calculateAcceleration(input);
+		glm::vec3 anga = calculateAngularAcceleration(input);
+
+		angular_velocity += anga * dt;
+		glm::vec3 time_derivative = AngularVelocityToTimeDerivative(angular_velocity);
+		orientation = orientation * time_derivative;
+		velocity += dt * a;
+		position += dt * velocity;
 	}
 
 	void setMVP(glm::mat4 mvp)
